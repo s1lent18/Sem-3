bb050996099141fc643428fb3f7ff05a7f994252
s11ent18 <alijafar.rajani@gmail.com>
2023-08-30 21:17:01 +0500

    modified QuizPrep

--- QuizPrep/DSA/Week1Quizprep.cpp
+++ QuizPrep/DSA/Week1Quizprep.cpp
@@ -3,6 +3,34 @@
 #include <vector>
 using namespace std;
 
+// Copy Constructor vs Assignment Operator
+
+// copy constructor is called when a new object is created from an existing object
+// as a copy of the existing object
+
+// assignment operator is called when an already initialized object is assigned
+// a new value form another existing object
+
+// Deep Copy vs Shallow Copy
+
+// Deep Copy:
+//      The Copy constructor is basically an overloaded constructor
+//      This initializes the new object with already existing object
+//      Copy Constructor is used when a new object is created with some existing object
+//      Both the objects use seperate memory locations
+//      if no copy constructor is defined in the class the compier provides one
+
+// Assignment Operator:
+//      Assignment operator is basically an operator
+//      this assigns the value of one object to another object both of which already exists
+//      This operator is used when we want to assign existing object to new object
+//      one memory location is used but different reference variables are pointing to the same location 
+//      if the assignment operator is not overloaded then bitwise copy will be made
+//Rule of Three
+
+
+
+
 class Prep
 {
     private:
@@ -16,7 +44,7 @@ class Prep
             data = 0;
         }
 
-        Prep(int data)                                     // Parameritized Constuctor
+        Prep(int data):data(data) // <- initializer list   // Parameritized Constuctor
         {
             this->data = data;
         }
