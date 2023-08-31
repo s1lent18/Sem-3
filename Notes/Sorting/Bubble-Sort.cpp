b8dbb8abf924848b319df37246cb45a496efe002
Ali <76154840+s1lent18@users.noreply.github.com>
2023-08-31 20:35:25 +0500

    Update Bubble-Sort.cpp

--- Notes/Sorting/Bubble-Sort.cpp
+++ Notes/Sorting/Bubble-Sort.cpp
@@ -13,12 +13,35 @@ using namespace std;
 // the reason to call it bubble sort is the smallest element rises to the beginning of the array
 // while the larger elements sink to the end of the array
 
+// Time Complexity
+// Best Case:       O(N)
+// Average Case:    O(N^2)
+// Worst Case:      O(N^2)
+
+// Space Complexity
+// O(1)
+
+// Advantages of Bubble Sort:
+//  - Bubble sort is easy to understand and implement
+//  - In place: it does not require any additional memory space
+//  - It is a stable sorting algorithm, meaning that elements with the same key
+//    value maintains their relative order in the sorted output
+
+// Disadvantages of Bubble Sort:
+//  - The main disadvantages is the amount of time it takes. It is highly inefficient
+//    for large data sets, with a running time of O(N^2). Futhermore,  the presence
+//    of turtles can significantly slow the sort
+
 void bubblesort(vector <int> vec)
 {
     int count = 1;
 
+    bool flag = false;
+
     while (count < vec.size())
     {
+        flag = false;
+
         for (int i = 0; i < (vec.size() - count); i++)
         {
             if (vec[i] > vec[i + 1])    // checking whether the element on the left is greater than the element on the right
@@ -28,8 +51,15 @@ void bubblesort(vector <int> vec)
                 vec[i] = vec[i + 1];
 
                 vec[i + 1] = temp;
+
+                flag = true;
             }
             
+        }
+
+        if(flag == false)
+        {
+            break;
         }
         count++;
     }
