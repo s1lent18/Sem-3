cf0e55a000f7320f2b41f17eff25b08bcd3d39f6
s11ent18 <alijafar.rajani@gmail.com>
2023-09-05 20:18:40 +0500

    Quick-Sort

---
+++ Notes/Sorting/Advanced-Sorting-Techniques/Quick-Sort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int left, int right)
+{
+    int pivot = arr[right];
+
+    int i = left - 1;
+
+    for(int j = left; j < right; j++)
+    {
+        if(arr[j] < pivot)
+        {
+            i++;
+
+            swap(arr[j], arr[i]);
+        }
+    }
+
+    swap(arr[i + 1], arr[right]);
+
+    return i + 1;
+}
+
+void QuickSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int pivot = partition(arr, left, right);
+
+        QuickSort(arr, left, pivot - 1);
+
+        QuickSort(arr, pivot + 1, right);
+    }
+}
+
+void print(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+
+int main()
+{
+    int arr[5] = {1, 32, 4, 54, 2};
+
+    QuickSort(arr, 0, 4);
+
+    print(arr, 5);
+}
